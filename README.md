65c046ad2315d777f482f1bfd390f1d5c031fbd6
rodobkn <rrmendoza@uc.cl>
2023-06-02 17:51:38 -0400

    subida de ejemplos

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # tutorial-python-1
+
+Acá encontrarán todos los recursos de mi primer tutorial de python
+en Youtube
---
+++ lista3.py
@@ -0,0 +1,3 @@
+listaSueldos = [[1500000, "Proveedor de Vegetales y Frutas"], [3000000, "Proveedor de Carbohidratos y Proteinas"], [800000, "Garzon 1"], [800000, "Garzon 2"], [1000000, "Chef 1"], [1000000, "Chef 2"], [900000, "Cajero"], [1200000, "Contador"]]
+
+nombres = ["Juan", "María", "Carlos", "Ana", "Pedro", "Laura", "Luis", "Sofía"]
